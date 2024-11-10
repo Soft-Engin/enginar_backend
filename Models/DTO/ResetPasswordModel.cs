85732aa173b8c25fb0b0d81423cf2c0a90f54355
ouzayb <ouzayb@gmail.com>
2024-11-10 15:20:56 +0300

    Update model annotations

--- Models/DTO/ResetPasswordModel.cs
+++ Models/DTO/ResetPasswordModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace Models.DTO
 {
     public class ResetPasswordModel
     {
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Token { get; set; }  // This will be the code sent to the user's email
+        [Required]
+        [MinLength(6)]
         public string NewPassword { get; set; }
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
