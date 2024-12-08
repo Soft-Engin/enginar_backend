70daa9a3045c1d36994fdd513f7ba348527cf0da
MustafaKrc <mustafa.kirci0@gmail.com>
2024-12-08 17:03:00 +0300

    Group UserDTOs into single file and delete deprecated ones

--- Models/DTO/UserDTO.cs
+++ Models/DTO/UserDTO.cs
@@ -18,4 +18,57 @@ namespace Models.DTO
         public string Email { get; set; }
         public string? PhoneNumber { get; set; }
     }
+
+    public class GetUserByIdDTO
+    {
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AddressName { get; set; }
+        public string Street { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int PostCode { get; set; }
+        public string RoleName { get; set; }
+
+    }
+
+    public class UpdateUserDto
+    {
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? AddressName { get; set; }
+        public string? Street { get; set; }
+        public string? District { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public int PostCode { get; set; }
+        public string? PhoneNumber { get; set; }
+
+    }
+
+    public class BookmarkRecipesItemDTO
+    {
+        public string UserName { get; set; }
+
+        public string Header { get; set; }
+
+        public string BodyText { get; set; }
+    }
+
+    public class BookmarkBlogsItemDTO
+    {
+        public string UserName { get; set; }
+        public string Header { get; set; }
+        public string BodyText { get; set; }
+    }
+
 }
