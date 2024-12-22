c7bafdaec83de814dd3d5e0a5564a77b0cb847db
ouzayb <ouzayb@gmail.com>
2024-12-22 04:03:21 +0300

    Extend feed provider repository

--- DataAccess/Repositories/IRepositories/IFeedProviderRepository.cs
+++ DataAccess/Repositories/IRepositories/IFeedProviderRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace DataAccess.Repositories.IRepositories
+﻿using System.Linq.Expressions;
+
+namespace DataAccess.Repositories.IRepositories
 {
     public interface IFeedProviderRepository<T> where T : class
     {
         Task<(IEnumerable<T> Items, int TotalCount)> GetPaginatedBySeedAsync(uint multiplier, uint seedValue, uint limiter, int pageNumber = 1, int pageSize = 10);
+        Task<(IEnumerable<T> Items, int TotalCount)> GetPaginatedBySeedAsync(uint multiplier, uint seedValue, uint limiter, int pageNumber = 1, int pageSize = 10, string includeProperties = "");
+        Task<(IEnumerable<T> Items, int TotalCount)> GetPaginatedBySeedAsync(uint multiplier, uint seedValue, uint limiter, Expression<Func<T, bool>> predicate, int pageNumber = 1, int pageSize = 10, string includeProperties = "");
     }
 }
