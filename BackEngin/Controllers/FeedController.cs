825b618d4ce04c04b7cda7c8ec814c6cbdff8a91
ouzayb <ouzayb@gmail.com>
2024-12-26 02:29:03 +0300

    Add Recent followed blog, recipe, event and upcoming followed event endpoints

--- BackEngin/Controllers/FeedController.cs
+++ BackEngin/Controllers/FeedController.cs
@@ -10,7 +10,7 @@ namespace BackEngin.Controllers
     [ApiVersion("1.0")]
     [Route("api/v{version:apiVersion}/feed")]
     [ApiController]
-    public class FeedController : ControllerBase
+    public class FeedController : ApiControllerBase
     {
         private readonly IFeedService _feedService;
 
@@ -65,6 +65,79 @@ namespace BackEngin.Controllers
                 if (pageSize <= 0) pageSize = 10;
 
                 var paginatedIngredients = await _feedService.GetEventFeed(seed, pageNumber, pageSize);
+                return Ok(paginatedIngredients);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An unexpected error occurred.", details = ex.Message });
+            }
+        }
+
+        // GET /ingredients - Get paginated list of Recipes
+        [HttpGet("recipe/recent-followed-feed")]
+        [Authorize]
+        public async Task<IActionResult> GetPaginatedRecentFollowedRecipes([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                if (pageNumber <= 0) pageNumber = 1;
+                if (pageSize <= 0) pageSize = 10;
+
+                var paginatedIngredients = await _feedService.GetFollowedRecentRecipeFeed(pageNumber, pageSize, await GetActiveUserId());
+                return Ok(paginatedIngredients);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An unexpected error occurred.", details = ex.Message });
+            }
+        }
+
+        // GET /ingredients - Get paginated list of Blogs
+        [HttpGet("blog/recent-followed-feed")]
+        public async Task<IActionResult> GetPaginatedRecentFollowedBlogs([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                if (pageNumber <= 0) pageNumber = 1;
+                if (pageSize <= 0) pageSize = 10;
+
+                var paginatedIngredients = await _feedService.GetFollowedRecentBlogsFeed(pageNumber, pageSize, await GetActiveUserId());
+                return Ok(paginatedIngredients);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An unexpected error occurred.", details = ex.Message });
+            }
+        }
+
+        // GET /ingredients - Get paginated list of Events
+        [HttpGet("event/recent-followed-feed")]
+        public async Task<IActionResult> GetPaginatedRecentFollowedEvents([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                if (pageNumber <= 0) pageNumber = 1;
+                if (pageSize <= 0) pageSize = 10;
+
+                var paginatedIngredients = await _feedService.GetFollowedRecentEventFeed(pageNumber, pageSize, await GetActiveUserId());
+                return Ok(paginatedIngredients);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An unexpected error occurred.", details = ex.Message });
+            }
+        }
+
+        // GET /ingredients - Get paginated list of Events
+        [HttpGet("event/upcoming-followed-feed")]
+        public async Task<IActionResult> GetPaginatedUpcomingFollowedEvents([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                if (pageNumber <= 0) pageNumber = 1;
+                if (pageSize <= 0) pageSize = 10;
+
+                var paginatedIngredients = await _feedService.GetFollowedUpcomingEventFeed(pageNumber, pageSize, await GetActiveUserId());
                 return Ok(paginatedIngredients);
             }
             catch (Exception ex)
