edf0c2297a71742e5e509ed66b214f89e1d10310
Zeynep Kara <karaz20@itu.edu.tr>
2024-12-30 00:34:36 +0300

    Addded Event Interactions
    Added database table models, repositories and unitofwork relations for event interactions

---
+++ DataAccess/Repositories/Event_LikesRepository.cs
@@ -0,0 +1,21 @@
+﻿using BackEngin.Data;
+using DataAccess.Repositories.IRepositories;
+using Models.InteractionModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class Event_LikesRepository : Repository<Event_Likes>, IEvent_LikesRepository
+    {
+        private readonly DataContext _db;
+        public Event_LikesRepository(DataContext db) : base(db)
+        {
+            _db = db;
+
+        }
+    }
+}
---
+++ Models/InteractionModels/Event_Likes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.InteractionModels
+{
+    public class Event_Likes
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public Users User { get; set; }
+
+        [Required]
+        public int EventId { get; set; }
+        [ForeignKey("EventId")]
+        public Events Event { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
