acc26d8be2b4c6be5bcfe22f19c0c86516ac0e34
Tamer Küçükel <tamerkucukel@hotmail.com>
2024-12-30 22:14:16 +0300

    Added AWS deployment with terraform

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -1,16 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.82.2"
     }
+  }
+
+  backend "s3" {
+    bucket         = "enginar-tf-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "enginar-tf-state-lock"
+    encrypt        = true
   }
 }
 
 provider "aws" {
-  region = var.aws_region
-  profile = var.aws_profile
+  region  = var.aws_region
 }
+
 
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
@@ -18,9 +26,10 @@ module "ec2_instance" {
 
   name = "enginar-project"
 
-  instance_type          = "t2.micro"
-  ami = data.aws_ami.amazonlinux_2023.id
-  key_name               = "enginar-key"
-  vpc_security_group_ids = [aws_security_group.allow_access.id]
+  instance_type               = var.instance_type
+  ami                         = data.aws_ami.amazonlinux_2023.id
+  key_name                    = "enginar-key"
+  vpc_security_group_ids      = [aws_security_group.allow_access.id]
   associate_public_ip_address = true
+  monitoring = true
 }
--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -8,32 +8,14 @@ variable "aws_region" {
   default     = "eu-central-1"
 }
 
-variable "aws_profile" {
-  type        = string
-  description = "AWS profile to use"
-  default     = "default"
-}
-
 #####################################################
 # Variables for the EC2 instance
 #####################################################
-
-variable "ami_id" {
-  type        = string
-  description = "AMI ID to use for the web server."
-  default     = data.aws_ami.amazonlinux_2023.id
-}
 
 variable "instance_type" {
   type        = string
   description = "EC2 instance type."
   default     = "t2.micro"
-}
-
-variable "key_name" {
-  type        = string
-  description = "Name of the existing EC2 Key Pair to associate with the instance."
-  default     = "instance-key"
 }
 
 
